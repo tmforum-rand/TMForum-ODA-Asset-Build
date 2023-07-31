c63c9a511d5556f5670e1ec891dd041d1a2312b9
Victor Mari Rodriguez <vrodriguez@tmforum.org>
2023-07-31 21:49:36 +0200

    Clean up

--- reference-implementations/TMF656_Service_Problem_ri/TMF656_Service_Problem/controllers/ProblemGroup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var ProblemGroup = require('../service/ProblemGroupService');
-
-module.exports.createProblemGroup = function createProblemGroup (req, res, next) {
-  ProblemGroup.createProblemGroup(req, res, next);
-};
--- reference-implementations/TMF673-GeographicAddress-v4/TMF673_GeographicAddress/controllers/NotificationListenersClientSide.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var NotificationListenersClientSide = require('../service/NotificationListenersClientSideService');
-
-module.exports.listenToGeographicAddressValidationStateChangeEvent = function listenToGeographicAddressValidationStateChangeEvent (req, res, next) {
-  NotificationListenersClientSide.listenToGeographicAddressValidationStateChangeEvent(req, res, next);
-};
