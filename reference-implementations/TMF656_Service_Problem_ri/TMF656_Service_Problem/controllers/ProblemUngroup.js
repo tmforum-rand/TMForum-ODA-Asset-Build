44ca5ff7fb99f7cbb223fa9c2a68766a35b6748a
Victor Mari Rodriguez <vrodriguez@tmforum.org>
2023-08-01 13:24:44 +0200

    Moved implementations

--- reference-implementations/TMF656_Service_Problem_ri/TMF656_Service_Problem/controllers/ProblemUngroup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var url = require('url');
-
-var ProblemUngroup = require('../service/ProblemUngroupService');
-
-module.exports.createProblemUngroup = function createProblemUngroup (req, res, next) {
-  ProblemUngroup.createProblemUngroup(req, res, next);
-};
